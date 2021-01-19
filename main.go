b0f3dc2a4574d6c83ffa7baa8347cf66b5d1289c
vikram-fit <vikramt@fitternity.com>
2021-01-20 00:02:54 +0530

    :zap: basic web server

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Println("New request has been made")
+	fmt.Fprint(w, "<h1> Welcome to my site </h1>")
+}
 
 func main() {
-	fmt.Println("Hello World!!")
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", nil)
+	// fmt.Println("Hello World!!")
 }
