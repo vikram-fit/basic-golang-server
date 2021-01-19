d3525dcd43124955258f964e59229cc2724b3d0c
vikram-fit <vikramt@fitternity.com>
2021-01-20 00:24:07 +0530

    :sparkles: Page not found

--- main.go
+++ main.go
@@ -8,9 +8,14 @@ import (
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	if r.URL.Path == "/" {
+		w.WriteHeader(http.StatusOK)
 		fmt.Fprint(w, "<h1> Welcome to my site </h1>")
 	} else if r.URL.Path == "/contact" {
+		w.WriteHeader(http.StatusOK)
 		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:vikramt@fitternity.com\">Vikram T</a>")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1> 404 Page Not Found </h1>")
 	}
 	// fmt.Println("New request has been made")
 }
